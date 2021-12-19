91f8122566bb97d87d2896a5983df4257dee60f7
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2021-12-20 00:21:07 +0530

    Created a basic structure with css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return <footer>Copyright ⓒ {currentYear}</footer>;
+}
+
+export default Footer;
